5b9d73924589c607b41759e6e71b0a06a6eca827
Adi Sulistiono <adi@artivisi.com>
2012-03-18 22:44:44 +0700

    implement jdbc di project

--- my-app/src/main/java/belajar/jdbc/App.java
+++ my-app/src/main/java/belajar/jdbc/App.java
@@ -1,13 +1,29 @@
 package belajar.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ){
+        try {
+            //parameter yang dikirim adalah host, port, dbname, dbuser, dbpaswd
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/belajar","root","admin");
+            String query = "insert into mahasiswa (nim, nama) values (?, ?)";
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, "12101022");
+            st.setString(2, "Djoko Susanto");
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
